16c3bac14cd23cdc6502ef94529ffe0082fee2b8
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-12 21:16:04 +0300

    Add files via upload

---
+++ problem_25.py
@@ -0,0 +1,8 @@
+# write a python program to get the difference between two sets =>
+frist_set = {1 , 12 , 2 , 6 , 7 , 8}
+second_set = {15 , 0 , 1 , 3 , 6}
+print("the different values of the differentn sets is : ")
+print("the frist set is : "+str(frist_set))
+print("the second set is : "+str(second_set))
+difference_set = frist_set . difference(second_set)
+print("the difference set between the two sets is : "+str(difference_set))
