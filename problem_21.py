d231efe1e16b875da4d4fc2580871af6b43d81b5
MahmoudKhalidShabaanOmar2 <mksomar200@gmail.com>
2025-02-12 19:10:59 +0300

    Add files via upload

---
+++ problem_21.py
@@ -0,0 +1,8 @@
+# write a python program to get the intersection between two sets =>
+frist_set = {5 , 2 , 4 , 6 , 7 , 1}
+second_set = {5 , 3 , 11}
+print("the different values of the different sets is : \n")
+print("the frist set is : "+str(frist_set))
+print("the second set is : "+str(second_set))
+intersection_set = frist_set . intersection(second_set)
+print("the intersection set between the two sets is : "+str(intersection_set))
