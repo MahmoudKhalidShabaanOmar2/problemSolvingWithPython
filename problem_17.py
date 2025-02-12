224f47243f202fc6290f79b9c0cfa71167af2ef8
MahmoudKhalidShabaanOmar2 <mksomar200@gmail.com>
2025-02-12 16:33:00 +0300

    Add files via upload

---
+++ problem_17.py
@@ -0,0 +1,8 @@
+# write a python program to get the union of the two sets => 
+frist_set = {3 , 2 , 4 , 5 , 6 , 7 , 8}
+second_set = {4 , 12 , 5 , 1 , 6 , 8}
+union_set = frist_set . union(second_set)
+print("the different values of the different sets is : \n")
+print("the frist set is : "+str(frist_set))
+print("the second set is : "+str(second_set))
+print("the union of the two sets is : "+str(union_set))
