88f4564fd0e658160dd8efcf977160914abe0b5e
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-13 21:09:43 +0300

    Add files via upload

---
+++ problem_41.py
@@ -0,0 +1,5 @@
+# write a python program to display the traingle by using "*" symboly without using space between "*" symbol :
+for i in range(5) :
+    for j in range(i + 1) :
+        print("*", end="")
+    print("")        
