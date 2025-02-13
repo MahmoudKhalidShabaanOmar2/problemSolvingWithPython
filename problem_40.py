898960a78d4010966f4aa82d9e852965253f2c63
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-13 20:34:09 +0300

    Add files via upload

---
+++ problem_40.py
@@ -0,0 +1,5 @@
+# write a python program to display traingle shape using "*" symbol by using loop by using space between "*" symbol :
+for i in range(5) :
+    for j in range(i + 1) :
+        print("*",end = " ")
+    print("")
