36873c0914395733b6c84dbf6a9321e3bda33823
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-14 15:47:53 +0300

    Add files via upload

---
+++ problem_47.py
@@ -0,0 +1,15 @@
+# write a python program that performs all compound assignment operations on an integer =>
+num = int(input("please enter the number is = "))
+print("the number is = "+str(num))
+num += 10
+print("the result of the addition compound assignment operation is = "+str(num))
+num -= 10
+print("the result of the subtraction compound assignment operation is = "+str(num))
+num *= 10
+print("the result of the multiplication compound assignment operation is = "+str(num))
+num /= 10
+print("the result of the division compound assignment operation is = "+str(num))
+num %= 10
+print("the result of the modulus compound assignment operation is = "+str(num))
+num **= 10
+print("the result of the square compound assignment operation is = "+str(num))
