8ff5f0e5468d31c3f5dbd1120608948e12b69ad9
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-14 19:29:54 +0300

    Add files via upload

---
+++ problem_52.py
@@ -0,0 +1,8 @@
+# write a python program to print message by using print function =>
+print("A")
+print("B C")
+print("D E F")
+print("G H I J")
+print("K L M N")
+print("O P Q R S")
+print("T U V W X Y")
