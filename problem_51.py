99c65723bae9c3de331a9b57c6169b9f81b830e4
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-14 18:37:04 +0300

    Add files via upload

---
+++ problem_51.py
@@ -0,0 +1,8 @@
+# write a python program to show a message in this format using print function , each character should have one tab distance =>
+print("A")
+print("B\tC")
+print("D\tE\tF")
+print("G\tH\tI\tJ")
+print("K\tL\tM")
+print("N\tO")
+print("P")
