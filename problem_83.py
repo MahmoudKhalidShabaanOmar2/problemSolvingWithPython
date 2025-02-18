881ee8adbb9c389728003a9c5f176d21edbb9b44
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-18 16:01:32 +0200

    Add files via upload

---
+++ problem_83.py
@@ -0,0 +1,7 @@
+# write a python program to get number from the user , and then check wether it is a positive , or negative number =>
+num = float(input("please enter the number is = "))
+print("the number is = "+str(num))
+if(num > 0) :
+    print("it is a positive number , because your entered number is = "+str(num))
+else :
+    print("it is a negative number , because your entered number is = "+str(num))
