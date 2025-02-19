35a16dd79ce1262b0e3077681fe0abb3764c5af8
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-19 17:29:30 +0200

    Add files via upload

---
+++ problem_99.py
@@ -0,0 +1,4 @@
+# write a python program to store color name in the list , and count that colors and display result to the user =>
+lista_of_colors_name = ["red" , "green" , "blue" , "white" , "black" , "orange" , "teal" , "tomato" , "pink" , "yellow"]
+print("the list of the different colors name is : "+str(lista_of_colors_name))
+print("the length of the list of the different colors is = "+str(len(lista_of_colors_name))+" colors")
