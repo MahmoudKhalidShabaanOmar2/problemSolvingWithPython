96b6d930226e8c76b8c15cf97c44d730dd8f08f5
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-20 13:44:53 +0200

    Add files via upload

---
+++ problem_101.py
@@ -0,0 +1,4 @@
+# write a python program to get different elements from the user in the list , and then count that element , and at the end display the result for the user =>
+lista_of_different_elements = ["mahmoud Khalid Shabaan Omar Awad Sallam" , "21 years" , "8 Nassar Nassif Kafr El El Gabal El Haram El Giza Egypt" , "computer science and information system" , "six october university"]
+print("the list of the different elements is : "+str(lista_of_different_elements))
+print("the length of the list of the different elements is = "+str(len(lista_of_different_elements))+" elements")
