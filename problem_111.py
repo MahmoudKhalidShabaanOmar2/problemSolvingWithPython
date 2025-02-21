636d257cb3c7157153cd8862a63800f6b6c4ef93
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-21 15:02:17 +0200

    Add files via upload

---
+++ problem_111.py
@@ -0,0 +1,5 @@
+# write a python program to get sentence from the user , and then convert sentence into capitalize sentence =>
+sentence = input("please enter the sentence is : ")
+print("the sentence is : \""+sentence+"\"")
+capitalize_sentence = sentence . capitalize()
+print("the capitalize of the sentence is : \""+capitalize_sentence+"\"")
