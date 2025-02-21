28d101610dfc7060cf352584289f2d7df90a500e
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-21 15:55:16 +0200

    Add files via upload

---
+++ problem_113.py
@@ -0,0 +1,5 @@
+# write a python program to get sentence from the user , and then get the uppercase of the sentence =>
+sentence = input("please enter the sentence is : ")
+print("the sentence is : \'"+sentence+"\'")
+upper_sentence = sentence . upper()
+print("the upper sentence is : \'"+upper_sentence+"\'")
