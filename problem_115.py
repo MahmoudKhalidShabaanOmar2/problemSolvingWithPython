5e811959c35ed972c4e1321f4adf51896914d39b
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-21 16:52:44 +0200

    Add files via upload

---
+++ problem_115.py
@@ -0,0 +1,7 @@
+# write a python program to get number from the user to get number from the user , and then get the square of the number =>
+num = int(input("please enter the number is = "))
+print("the number is = \t\""+str(num)+"\"")
+# square_of_num = num * num 
+# square_of_num = num ** 2
+square_of_num = pow(num , 2)
+print("the square of the number is = \t\"",str(square_of_num)+"\"")
