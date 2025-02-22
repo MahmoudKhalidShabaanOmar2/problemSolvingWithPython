3f364ed55570d450382366aeeebe5728122ccddd
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-22 15:15:51 +0200

    Add files via upload

---
+++ problem_121.py
@@ -0,0 +1,6 @@
+# write a python program to convert radian angle to degree angle =>
+import math 
+radian_angle = float(input("please enter the radian angle is = "))
+print("the radian angle is = "+str(radian_angle)+"rad")
+degree_angle = math . degrees(radian_angle)
+print("the degree angle is = "+str(degree_angle)+"deg")
