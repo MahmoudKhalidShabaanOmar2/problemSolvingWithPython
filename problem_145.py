9719e968fbef91f1f49825765fed2d2864b923e0
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-24 13:00:05 +0200

    Add files via upload

---
+++ problem_145.py
@@ -0,0 +1,9 @@
+# write a python program to display only odd numbers from 1 to 30 in the properly sequence =>
+# print("generating only odd numbers from starting number is \"1\" to the ending number is \"30\"")
+# for num in range(1 , 31) :
+#     if(num %2 != 0) :
+#         print(num)
+
+print("generating only odd numbers from starting number is = \"1\" to ending number is = \"30\"")
+for num in range(1 , 31 , 2) :
+    print(num)
