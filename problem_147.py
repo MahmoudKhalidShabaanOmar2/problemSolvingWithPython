946d6aa746afaf16ff8c60d98ba6e936f394fbda
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-24 13:45:32 +0200

    Add files via upload

---
+++ problem_147.py
@@ -0,0 +1,9 @@
+# write a python program to generate only even numbers from 1 to 30 =>
+# print("generating only even numbers from starting number is = \"1\" to ending number is = \"30\"")
+# for num in range(1 , 31) :
+#     if(num %2 == 0) :
+#         print(num)
+
+print("generating only even numbers from starting number is = \"0\" to ending number is = \"30\"")
+for num in range(0 , 31 , 2) :
+    print(num)
