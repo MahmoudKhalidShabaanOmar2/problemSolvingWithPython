941ad440a51c0f96d501b5a1b0704e1786bbd7fd
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-25 14:42:43 +0200

    Add files via upload

---
+++ problem_151.py
@@ -0,0 +1,13 @@
+# write a python program to get find two numbers from the user , and then find the square of the two number , and at the end find the sum of the square of the two numbers =>
+frist_num = int(input("please enter the frist number is = "))
+second_num = int(input("please enter the second number is = "))
+print("the frist number is = "+str(frist_num))
+print("the second number is = "+str(second_num))
+sum_of_frist_and_second_num = frist_num + second_num 
+print("the sum of the two numbers is = "+str(sum_of_frist_and_second_num))
+square_of_frist_num = frist_num * frist_num 
+print("the square of the frist number is = "+str(square_of_frist_num))
+square_of_second_num = second_num * second_num 
+print("the square of the second number is = "+str(square_of_second_num))
+sum_of_square_of_frist_num_and_square_of_second_num = square_of_frist_num + square_of_second_num 
+print("the sum of the square of the frist number , and square of the second number is = "+str(sum_of_square_of_frist_num_and_square_of_second_num))
