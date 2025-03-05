5f81395b537a4eef34c090ede2efbbe3ecc8fa3f
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-03-05 22:16:44 +0200

    Add files via upload

---
+++ problem_199.py
@@ -0,0 +1,6 @@
+# write a python program to get frist user name , and last user name , and then display the full name for the user =>
+frist_name = input("please enter the frist name of the user is : ")
+second_name = input("please enter the second name of the user is : ")
+print("the frist name is : "+frist_name)
+print("the second name is : "+second_name)
+print("the full name is : "+frist_name+" "+second_name)
