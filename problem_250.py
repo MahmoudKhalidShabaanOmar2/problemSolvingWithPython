15947c8c18928d2db24d16296cf3be2da1884de4
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-09 17:27:32 +0200

    Add files via upload

---
+++ problem_250.py
@@ -0,0 +1,7 @@
+# write a python program to get sentence from the user , display the last character from the sentence , and then display all characters of the sentence =>
+sentence = input("please enter the sentence is : ")
+print("the sentence is : "+sentence)
+print("the last character from the sentence is : "+sentence[-1])
+print("all characters of the sentence is : ")
+for i in sentence :
+    print(i)
