7746b9f6b69842621a064a5017fe03f55d22461b
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-14 14:52:23 +0200

    Add files via upload

---
+++ problem_265.py
@@ -0,0 +1,7 @@
+# write a python program to get a number from the user to return its next , and previous number from your entered number => 
+number = int(input("Please Enter The Number Is = "))
+print("The Number Is = ",str(number))
+previous_number = number - 1
+print("The Previous Number Is = ",str(previous_number))
+next_number = number + 1 
+print("The Next Number Is = ",str(next_number))
