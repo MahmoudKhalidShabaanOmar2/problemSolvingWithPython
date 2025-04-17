05eb9cec3dd616175437f2bcb1dbe94569ec732d
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-17 20:56:54 +0200

    Add files via upload

---
+++ problem_279.py
@@ -0,0 +1,7 @@
+# write a python program to get string sentence from the user , and then print the frist character , and last character from our string sentence => 
+word = input("Please Enter The word Is : ")
+print("The Sentence Is : ",word)
+frist_char_from_word = word[0]
+print("The Frist Character From The word Is : "+frist_char_from_word)
+last_char_from_word = word[-1]
+print("The Last Character From The Word Is : "+last_char_from_word)
