03be9f78ca76fe9651043c0eff3d7f152b6ef5fd
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-19 16:37:28 +0200

    Add files via upload

--- problem_289.py
+++ problem_289.py
@@ -1,1 +1,8 @@
-# write a python 
+# write a python program to get name from the user , to check it is in uppercase , or not , if not upper then converted to the uppercase =>
+user_name = input("Please Enter The Name Of The User Is : ")
+print("The Name Of The User Is : \""+user_name+"\"")
+if(user_name.isupper()) :
+    print("The User Name In The Uppercase")
+else :
+    print("The User Name In Lowercase")
+    print("The User Name In The Uppercase Is : "+user_name.upper())
