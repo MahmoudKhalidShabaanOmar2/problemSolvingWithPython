6e8959dbb2fe36442a53720fd4196e7a7ffe7b1d
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-21 14:46:01 +0200

    Add files via upload

---
+++ problem_299.py
@@ -0,0 +1,7 @@
+# write a python program to get the number from the user , the system should auto decrement to that number , half of user entered number should be decremented =>
+number = int(input("Please Enter The Number Is = "))
+print("The Number Is = ",str(number))
+half_count_number = number / 2
+print("The Half Count Number Is = ",str(half_count_number))
+sub_num_half_num = number - half_count_number 
+print("The Subtraction Of The Number , And The Half Count Number Is = ",sub_num_half_num)
