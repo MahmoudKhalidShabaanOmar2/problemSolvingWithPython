87a5a7d2c353012defb11f95048a5e823daf680a
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-21 14:14:39 +0200

    Add files via upload

---
+++ problem_298.py
@@ -0,0 +1,7 @@
+# write a python program to get the number from the user , the system should add auto increment to that half number of user entered number should be incremented => 
+number = int(input("Please Enter The Number Is = "))
+print("The Number Is = ",str(number))
+half_number = number / 2
+print("The Half Of ",str(number)," Is = ",str(half_number))
+add_num_half_num = number + half_number 
+print("The Sum Of The Number , And Half Number Is = ",str(add_num_half_num))
