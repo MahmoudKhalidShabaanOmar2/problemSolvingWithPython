7c3ab03496c11bc2eb7a03d336c180cad9a389a3
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-23 21:21:18 +0200

    Add files via upload

---
+++ problem_306.py
@@ -0,0 +1,7 @@
+#   write a python program to get the mass of an object through the density of an object =>
+densityOfObject = int(input("Please Enter The Density Of An Object Is = "))
+volumeOfObject = int(input("Please Enter The Density Of An Object Is = "))
+print("The Density Of The Object Is = ",str(densityOfObject))
+print("The Volume Of The Object Is = ",str(volumeOfObject))
+massOfObject = densityOfObject * volumeOfObject 
+print("The Mass Of An Object Is = ",str(massOfObject))
