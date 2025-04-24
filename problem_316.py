e8ab12c8053dbb5b1180c6e8a28f32de42bc6986
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-24 19:09:00 +0200

    Add files via upload

---
+++ problem_316.py
@@ -0,0 +1,6 @@
+# write a python program to get acceleration of an object , that having mass , and force of an object from the user => 
+mass_of_object = int(input("Please Enter The Mass Of An Object Into Kilograma Is = "))
+force_of_object = int(input("Please Enter The Force Of An Object Into Newoton Is = "))
+print("The Mass Of An Object Is = ",str(mass_of_object)," Kilograma")
+acceleration_of_object = force_of_object / mass_of_object 
+print("The Acceleration Of An Object Is = ",str(acceleration_of_object)," Meter/Second2")
