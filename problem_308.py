20679f0992b1dea3f96b975e9fda81489cce6871
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-24 16:54:04 +0200

    Add files via upload

---
+++ problem_308.py
@@ -0,0 +1,6 @@
+# write a python program to get number from the user , and then getting the celling of the number => 
+import math 
+number = float(input("Please Enter The Number Is = "))
+print("The Number Is = ",number)
+ceilingOfNumber = math . ceil(number)
+print("The Ceiling Of The Number Is = ",ceilingOfNumber," , Because Your Entered Number Is = ",number)
