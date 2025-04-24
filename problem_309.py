f3df1c5f99ba2ca0db63c5398e891295215c4ed4
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-24 17:02:46 +0200

    Add files via upload

---
+++ problem_309.py
@@ -0,0 +1,6 @@
+# write a python program to get number , and then get the floor of the number =>
+import math 
+number = float(input("Please Enter The Number Is = "))
+print("The Number Is = ",number)
+floorOfNumber = math . floor(number)
+print("The Floor Of The Number Is = ",floorOfNumber," , Because Your Entered Number Is = ",number)
