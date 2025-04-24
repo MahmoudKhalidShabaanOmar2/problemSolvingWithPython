4a2184c0cc8fd839e61437e9999cb613a85b8640
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-24 18:51:12 +0200

    Add files via upload

---
+++ problem_315.py
@@ -0,0 +1,7 @@
+# write a python program to find mass of an object that accelerated by man => 
+force_of_object = int(input("Please Enter The Force Of An Object Into Newoton Is = "))
+acceleration_of_object = int(input("Please Enter The Acceleration Of An Object Into Meter/Second2 Is = "))
+print("The Force Of An Object Is = ",str(force_of_object)," Newoton")
+print("The Acceleration Of An Object Is = ",str(acceleration_of_object)," Meter/Seconds")
+mass_of_object = force_of_object / acceleration_of_object 
+print("The Mass Of An Object Is = ",str(mass_of_object)+" Kilograma")
