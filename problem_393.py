18d5931cf83673b0c0100b9755307a58eaa68a43
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-05-28 18:17:27 +0300

    Add files via upload

---
+++ problem_393.py
@@ -0,0 +1,6 @@
+# write a python program to calcuate the circumference of the circle =>
+import math 
+radius_of_circle = float(input("Please Enter The Radius Of The Circle Is = "))
+print("The Radius Of The Circle Is = ",str(radius_of_circle))
+circumference_of_circle = 2 * math.pi * radius_of_circle 
+print("The Circumference Of The Circle Is = ",str(circumference_of_circle))
