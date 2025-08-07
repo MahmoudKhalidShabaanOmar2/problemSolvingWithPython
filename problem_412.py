ae38e7f64f750a78b502c367ca5ebab003abb5a1
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-08-07 23:34:09 +0300

    Add files via upload

---
+++ problem_412.py
@@ -0,0 +1,10 @@
+# write a python program to sort the dictionary by the key =>
+dictionary_of_user_information = {
+    "name" : "mahmoud",
+    "salary" : "01000",
+    "age" : "22",
+    "faculty" : "computer Science , And Information System",
+    "university" : "October 6 University"
+}
+print("The Dictionary Of The User Information Is : ",str(dictionary_of_user_information))
+print(sorted(dictionary_of_user_information.items()))
