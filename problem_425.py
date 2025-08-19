44c233f1a670811d40111105a6b905b76e5ee961
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-08-19 23:43:40 +0300

    Add files via upload

---
+++ problem_425.py
@@ -0,0 +1,5 @@
+# write a python program to create a nested tuple =>
+nested_list = [1 , 2 , 3 , 5 , ["Ahmed" , "Mohamed"] , [True , False] , [1 , 2 , 4 , 5] , [-2 , -5 , -534 , -646] , [1.343 , 54.5656 , 35.5757] , [-24.3535 , -35.574 , -464.3433]]
+print("The Nested List Is : ",str(nested_list))
+nested_tuple = tuple(nested_list)
+print("The Nested Tuple Is : ",str(nested_tuple))
