3844a24db848ebe2753bbc9d1fccc899c39a1df4
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-08-19 23:40:18 +0300

    Add files via upload

---
+++ problem_424.py
@@ -0,0 +1,9 @@
+# write a python program to access different list from the nested list =>
+nested_list = [1 , 2 , 3 , [True , False] , ["Ahmed" , "Mohamed"] , [1 , 2 , 3 , 5] , [-1 , -5 , -78] , [1.34 , 343.35 , 35.5646] , [-242.3535 , -35.4646]]
+print("The Nested List Is : ",str(nested_list))
+print("The Access List From The Nested List Is : ",nested_list[3])
+print("The Access List From The Nested List Is : ",nested_list[4])
+print("The Access List From The Nested List Is : ",nested_list[5])
+print("The Access List From The Nested List Is : ",nested_list[6])
+print("The Access List From The Nested List Is : ",nested_list[7])
+print("The Access List From The Nested List Is : ",nested_list[8])
