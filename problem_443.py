24a34d7ab2fe564e3fd47b3a5c015f6e2c8fbba7
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-08-31 23:58:05 +0300

    Add files via upload

--- problem_443.py
+++ problem_443.py
@@ -1,7 +1,9 @@
 # write a python program to get number from the user , and then get the previous , and next number by using function =>
 number = int(input("Please Enter The Number Is = "))
-print("The Number Is = ",str(number))
-previous_number = number - 1
-print("The Previous Number Is = ",str(previous_number))
-next_number = number + 1
-print("The Next Number Is = ",str(next_number))
+def getPreviousAndNextNumber(num): 
+    print("The Number Is = ",str(num))
+    previous_number = num - 1
+    print("The Previous Number Is = ",str(previous_number))
+    next_number = num + 1
+    print("The Next Number Is = ",str(next_number))
+getPreviousAndNextNumber(number)
