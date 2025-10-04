ebe2c3150b447b20f8aad7633eae9ec754222a06
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-10-04 08:11:43 +0300

    Add files via upload

--- problem_14.py
+++ problem_14.py
@@ -1,31 +1,7 @@
-# write a python program to get two numbers from the user , and then make all arthimetic operations on the two numbers by using function =>
-# frist_number = float(input("please enter the frist number is = "))
-# second_number = float(input("please enter the second number is = "))
-# def get_all_arthimetic_operations_on_two_numbers(fri_num , sec_num) :
-#     print("the values of the two numbers is : \n")
-#     print("the frist number is = "+str(fri_num))
-#     print("the second number is = "+str(sec_num))
-#     print("the arthimetic operations on the two numbers is : \n")
-#     print("the result of the addition arthimetic operation on the two numbers is = "+str(fri_num + sec_num))
-#     print("the result of the subtraction arthimetic operation on the two numbers is = "+str(fri_num - sec_num))
-#     print("the result of the multiplication arthimetic operation on the two numbers is = "+str(fri_num * sec_num))
-#     print("the result of the division arthimetic operation on the two numbers is = "+str(fri_num / sec_num))
-#     print("the result of the modulus arthimetic operation on the two numbers is = "+str(fri_num % sec_num))
-# get_all_arthimetic_operations_on_two_numbers(frist_number , second_number)
-
-def get_all_arthimetic_operations_on_two_numbers(fri_num , sec_num) :
-    print("the values of the two numbers is : \n")
-    print("the frist number is = "+str(fri_num))
-    print("the second number is = "+str(sec_num))
-    print("the arthimetic operations on the two numbers is : \n")
-    print("the result of the addition arthimetic operation on the two numbers is = "+str(fri_num + sec_num))
-    print("the result of the subtraction arthimetic operation on the two numbers is = "+str(fri_num - sec_num))
-    print("the result of the multiplication arthimetic operation on the two numbers is = "+str(fri_num * sec_num))
-    print("the result of the division arthimetic operation on the two numbers is = "+str(fri_num / sec_num))
-    print("the modulus of the modulus arthimetic operation of the two numbers is = "+str(fri_num % sec_num))
-frist_number = float(input("please enter the frist number is = "))
-second_number = float(input("please enter the second number is = "))
-arthimetic_operations_on_two_numbers = get_all_arthimetic_operations_on_two_numbers(frist_number , second_number)
-print(arthimetic_operations_on_two_numbers)
-    
-    
+# write a python program to get file name with extenstion , and then displaying the file extenstion only =>
+import os 
+file_name_with_file_extenstion = input("Please Enter The File Name With File Extenstion Is : ")
+print("The File Name With The File Extenstion Is : ",file_name_with_file_extenstion)
+file_name , file_extenstion = os.path.splitext(file_name_with_file_extenstion)
+print("The File Name Is : ",file_name)
+print("The File Extenstion Is :"+file_extenstion)
