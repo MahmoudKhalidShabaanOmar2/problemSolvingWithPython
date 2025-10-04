4f1ae68f45a41b59d4e426d55208ca6880432e28
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-10-04 08:35:01 +0300

    Add files via upload

---
+++ problem_446.py
@@ -0,0 +1,10 @@
+# write a python program to calculate the circumference , and area of the circle =>
+import math 
+radius_of_circle = float(input("Please Enter The Radius Of The Circle Is = "))
+print("The Radius Of The Circle Is = ",str(radius_of_circle))
+circumference_of_circle = 2 * math.pi * radius_of_circle 
+print("The Circumference Of The Circle Is = ",str(circumference_of_circle))
+# area_of_circle = math.pi * radius_of_circle * radius_of_circle 
+# area_of_circle = math.pi * math.pow(radius_of_circle , 2)
+area_of_circle = math.pi * radius_of_circle ** 2
+print("The Radius Of The Circle Is = ",str(area_of_circle))
