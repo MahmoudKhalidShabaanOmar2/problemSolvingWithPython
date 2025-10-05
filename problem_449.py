98b8a3caa4441b96b426cd19920dc41416bed390
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-10-05 07:54:44 +0300

    Add files via upload

--- problem_449.py
+++ problem_449.py
@@ -1,10 +1,10 @@
 # write a python program to get the user password that contain alphanumeric characters , and the length of the user password is grater than eight , and less than twenty characters , and then restrict user if include uppercase letter , or white spaces =>
 user_password = input("Please Enter The User Password Is : ")
 print("The User Password Is : ",user_password)
-if(user_password.isalnum() and not user_password.isalpha() and not user_password.isdigit() and not user_password.upper() and not user_password.isspace()):
+if(not user_password.isdigit() and not user_password.isupper() and not user_password.isspace() and user_password.isalnum()):
     if(len(user_password) > 8 and len(user_password) < 20):
-        print("Hello , And Welccome Back")
-    else: 
-        print("Sorry , This Is Invalid User Password , Because The Length Of The User Password Is Less Than Eight Characters , Or Grater Than Twenty Characters.")
+        print("Okay , And Welcome Back")
+    else:
+        print("The User Password Is Invalie , Because The Length Of The User Password Is Grater Than Eight Characters , And Less Than Twenty Characters")
 else:
-    print("This Is Invalid User Password , Because The User Password Contains Alphabetic Characters , Numeric Characters Only , Capital Characters (UpperCase Characters) , or White Spaces.")
+    print("This Is Invalie User Password , Because The User Password Container White Spaces , Capital (Uppercase Characters) , Alphabetic Characters , Or Numeric Characters Only.")
