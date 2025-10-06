aca0e95fbaee610352728633a3baf2b7d742b6e0
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-10-06 21:14:49 +0300

    Add files via upload

---
+++ problem_454.py
@@ -0,0 +1,7 @@
+# write a python program to get the dictionary of the user information , and then get the total length of the keys , and values of the dictionary =>
+dictionary_of_user_information = {"name" : "Mahmoud Khalid" , "age" : "23 Years Old" , "gender" : "male"}
+print("The Dictionary Of The User Information Is : ",str(dictionary_of_user_information))
+length_of_keys = len(dictionary_of_user_information.keys())
+print("The Length Of The Keys Of The Dictionary of The User Information Is = ",str(length_of_keys)," Keys")
+length_of_values = len(dictionary_of_user_information.values())
+print("The Length of The Values Of The Dictionary Of The User Information Is = ",str(length_of_values)," Values")
