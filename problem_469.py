5300482676c06c3f7600f88ecf5c603a9d922af6
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-10-13 23:57:18 +0300

    Add files via upload

---
+++ problem_469.py
@@ -0,0 +1,5 @@
+# write a python program to aaaccess different list , or elements from the nested list =>
+nested_lista = [1 , 2 , 3 , [4 , 5 , 6] , "mahmoud" , "khalid" , [2.5 , -5 , -5.4] , True , False , ["amhmoud Khalid" , "23 Years Old" , "Computer Science , And Information System"] , ["False" , "true" , True , False]]
+print("The Nested List Of All Elements Is : ",str(nested_lista))
+print("The Second Element From The Nested List Of All Elements Is : ",str(nested_lista[3]))
+print("The Last Element From The Nested Elements From The List Of All Elements Is : ",str(nested_lista[-1]))
