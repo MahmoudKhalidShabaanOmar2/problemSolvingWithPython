b564a0a89c6bbfd86262c39c56c0d44c8d1eb027
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-10-18 22:33:11 +0300

    Add files via upload

---
+++ problem_474.py
@@ -0,0 +1,9 @@
+# write a python program to access multiple elements from the list =>
+lista = [12 , 434 , 35 , 565.67644 , 34.455 , -34343 , -6565 , -3434.45645 , -4545.454 , "Mahmoud Khalid" , "Male" , True , False , "true" , "false"]
+print("The List Is : ",str(lista))
+starting_position = int(input("Please Enter The Starting Position Is = "))
+print("The Starting Position Is = ",str(starting_position))
+ending_position = int(input("Please Enter The Ending Position Is = "))
+print("The Ending Position Is = ",str(ending_position))
+multiple_elements_from_list = lista[starting_position:ending_position + 1]
+print("The Multiple Elements From The List Is : ",str(multiple_elements_from_list))
