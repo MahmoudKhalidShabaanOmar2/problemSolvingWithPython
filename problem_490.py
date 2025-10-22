995e4703685a7c576de87a513eed68ac3dcb4cd5
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-10-22 23:57:40 +0300

    Add files via upload

---
+++ problem_490.py
@@ -0,0 +1,6 @@
+# write a python program to get nested dictionary from the user , and then sccess values =>
+nested_dictionary = {1: {"name" : "Mahmoud Khalid"} , 2: "Light Sky" , 3:{"age" : "23 Years Old"}}
+print("The Nested Dictionary Is : ",str(nested_dictionary))
+print("The Name From The Nested Dictionary Is : ",str(nested_dictionary[1]["name"]))
+print("The Color From The Nested Dictionary Is : ",str(nested_dictionary[2]))
+print("The Age From The Nested Dictionary Is = ",str(nested_dictionary[3]["age"]))
