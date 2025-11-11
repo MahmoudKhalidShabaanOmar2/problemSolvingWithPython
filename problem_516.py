cadbb24c05b695810bf93e03d2a82ca85c07a99c
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-11-11 22:19:50 +0300

    Add files via upload

---
+++ problem_516.py
@@ -0,0 +1,7 @@
+# write a python program to get the matrix of 2 identity by using function method =>
+import numpy 
+def get_matrix_of_2():
+    arr = numpy.identity(2)
+    print("The Matrix Of 2 Is : ")
+    print(arr)
+get_matrix_of_2()
