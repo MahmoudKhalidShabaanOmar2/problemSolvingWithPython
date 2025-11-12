287176b2e9dbff91667a4e623f329484dd220b61
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-11-12 14:44:10 +0300

    Add files via upload

---
+++ problem_519.py
@@ -0,0 +1,5 @@
+# write a python program to create the one dimenssional array by using arange built in function =>
+import numpy as np
+one_dimenssional_array = np.arange(1 , 10)
+print("The One Dimenssional Array Is : ")
+print(one_dimenssional_array)
