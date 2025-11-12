380c0af9623ea4c2516d1f7b3e7da153d73450fe
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-11-12 15:32:06 +0300

    Add files via upload

---
+++ problem_521.py
@@ -0,0 +1,5 @@
+# write a python program to create array of zeros by using zeros built in function =>
+import numpy as np
+one_dimenssional_array_of_zeros = np.zeros(10)
+print("The One Dimenssional Array Of Zeros Is : ")
+print(one_dimenssional_array_of_zeros)
