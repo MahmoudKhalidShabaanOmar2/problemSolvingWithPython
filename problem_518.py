f70978110aed7561db334e387b05e5cfc46fa11a
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-11-12 14:22:39 +0300

    Add files via upload

---
+++ problem_518.py
@@ -0,0 +1,8 @@
+# write a python to create one dimenssional array by using function method =>
+import numpy as np 
+def create_one_dimenssional_array(one_dimenssional_arr):
+    print("The One Dimenssional Array Is : ")
+    return one_dimenssional_arr 
+one_dimenssional_array = np.array([1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10])
+displaying_one_dimenssional_array = create_one_dimenssional_array(one_dimenssional_array)
+print(displaying_one_dimenssional_array)
