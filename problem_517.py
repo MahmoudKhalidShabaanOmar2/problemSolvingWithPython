9e2c37c64458f21baef0c2fcdd0708b169ee278e
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-11-12 14:05:36 +0300

    Add files via upload

---
+++ problem_517.py
@@ -0,0 +1,4 @@
+# write a python program to create one dimenssional array =>
+import numpy as np
+one_dimenssional_arr = np.array([1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10])
+print("The One Dimenssional Array Is : ",str(one_dimenssional_arr))
